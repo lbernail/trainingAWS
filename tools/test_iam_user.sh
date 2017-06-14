390413ecb68859eda5583b55fe925abb7fb543c4
cyrielle camanes <cyrielle.camanes@d2-si.eu>
2017-06-14 19:08:52 +0200

    adapt test script

--- tools/test_iam_user.sh
+++ tools/test_iam_user.sh
@@ -7,17 +7,14 @@ csv_credentials=$1
 # set region
 export AWS_DEFAULT_REGION=${2:-eu-west-1}
 
-group="admin"
-
 while read -r line || [ -n "$line" ]
-#while read -r line
 do
     # delete end of line
-    line=${line//[[:space:]]/,}
+    line=${line//[[:space:]]/;}
     # set variables
-    read url login passwd AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY trash <<< ${line//,/ }
-    echo "Testing account $login"
-
+    read url user passwd AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY trash <<< ${line//;/ }
+    echo "Testing account $user"
+    
     # export credentials
     export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
 
